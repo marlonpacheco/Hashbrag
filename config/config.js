c7b35bf966f6b9550c52486df459b898d3b4f0f3
Andrew Freire <andrew.m.freire@gmail.com>
2020-10-12 19:44:50 -0500

    Fixed CSS style on bootstrap buttons

--- config/config.js
+++ config/config.js
@@ -1,12 +1,7 @@
 module.exports = {
   "development": {
-<<<<<<< HEAD:config/config.json
-    "username": "root",
-    "password": "",
-=======
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
->>>>>>> c19b7d99401ad07765edcb1ee7d82210e66fa419:config/config.js
     "database": "hashbrag",
     "host": "127.0.0.1",
     "dialect": "mysql"
